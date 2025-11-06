62ba41c867c81e78f00ab17be7eaf891944392b6
Victormanova <victor.manova@srmtech.com>
2025-11-06 16:43:14 +0530

    end time for test

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -4,7 +4,7 @@ import { Heart, Sparkles } from "lucide-react";
 
 // EDITABLE: Wedding date and time (IST timezone)
 // const WEDDING_DATE = new Date("2026-01-12T10:00:00+05:30");
-const WEDDING_DATE = new Date("2025-11-11T16:45:00+05:30");
+const WEDDING_DATE = new Date("2025-11-06T16:45:00+05:30");
 
 interface TimeLeft {
   days: number;
